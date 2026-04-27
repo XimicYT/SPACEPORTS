559ca974e15f5e8c27bb2c26ea2c50e9daae7bac
XimicYT <abutts29@students.unorth.k12.in.us>
2026-04-27 08:59:38 -0400

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ const TILE_SIZE = 300;
 // "A little more than about half the width of a hallway"
 const BALL_RADIUS = TILE_SIZE / 4 + 10;
 const FRICTION = 1;
-const BOUNCE = -1.1; // How bouncy the walls are
+const BOUNCE = -1; // How bouncy the walls are
 
 const MAP_BLUEPRINT = [
   "111111111111111111111111111111111111111111111",
